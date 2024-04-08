398f5a535dd0056fb0f0b1c116417c9282b4cb50
unknown <quillaluis501@gmail.com>
2024-04-08 08:54:26 -0500

    gateway

---
+++ Msgetway-serve/src/main/java/com/example/msgetwayserve/MsgetwayServeApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgetwayserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgetwayServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgetwayServeApplication.class, args);
+	}
+
+}
---
+++ Msgetway-serve/src/test/java/com/example/msgetwayserve/MsgetwayServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgetwayserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgetwayServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
