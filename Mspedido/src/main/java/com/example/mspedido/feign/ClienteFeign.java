aded9731c4016e3f7ec8d548d16baf2a3e11f80b
unknown <quillaluis501@gmail.com>
2024-04-29 09:25:12 -0500

    Resiliencia

--- Mspedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
+++ Mspedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
@@ -1,6 +1,8 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ClientesDto;
+import com.example.mspedido.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ClienteFeign {
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "clientesListarPorIdCB", fallbackMethod = "fallBackCliente")
     public ResponseEntity<ClientesDto> listById(@PathVariable(required = true) Integer id);
+    default ResponseEntity<ClientesDto> fallBackCliente(Integer id, Exception e) {
+
+        return ResponseEntity.ok(new ClientesDto());
+    }
 }
--- Mspedido/src/main/java/com/example/mspedido/feign/ProductoFeign.java
+++ Mspedido/src/main/java/com/example/mspedido/feign/ProductoFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ProductoFeign {
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallBackProducto")
     public ResponseEntity<ProductoDto> listById(@PathVariable(required = true) Integer id);
+
+    default ResponseEntity<ProductoDto> fallBackProducto(Integer id, Exception e) {
+        ProductoDto productoDto = new ProductoDto();
+        productoDto.setId(400000);
+        return ResponseEntity.ok(productoDto);
+    }
 }
