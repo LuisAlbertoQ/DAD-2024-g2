2c5425fd1272c38d210d848fd1040a430cefb5cd
unknown <quillaluis501@gmail.com>
2024-05-13 10:52:21 -0500

    seguridad

--- ms-auth/src/main/java/com/example/msauth/repository/AuthRepository.java
+++ ms-auth/src/main/java/com/example/msauth/repository/AuthRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AuthRepository extends JpaRepository<AuthUser, Integer> {
 
-    Optional<AuthUser> findByUsername(String username);
+    Optional<AuthUser> findByUserName(String username);
 }
