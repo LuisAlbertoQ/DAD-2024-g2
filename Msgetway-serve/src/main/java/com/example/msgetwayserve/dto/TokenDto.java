70b519729c3bee86d625dbb60e29f88b07acfac1
unknown <quillaluis501@gmail.com>
2024-05-20 09:32:49 -0500

    seguridad, politicas y filtros

---
+++ Msgetway-serve/src/main/java/com/example/msgetwayserve/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package com.example.msgetwayserve.dto;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
